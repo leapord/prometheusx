2febf40e92af0c4cf525a49c895b5866d9463ce0
leapord <qiyuyang@taisau.com>
2022-09-09 09:49:42 +0800

    validate change

--- api/v1/authentication.go
+++ api/v1/authentication.go
@@ -7,7 +7,7 @@ import (
 type LoginReq struct {
 	g.Meta    `path:"/login" tags:"Login" method:"post" summary:"user login api"`
 	LoginName string `p:"loginName" v:"required" dc:"user's login name"`
-	Password  string `p:"password" v:"required" dc:"password"`
+	Password  string `p:"password" v:"password3" dc:"password"`
 }
 type LoginRes struct {
 	g.Meta `mime:"text/html" example:"success"`
@@ -18,8 +18,8 @@ type RegisterReq struct {
 	g.Meta      `path:"/regist" tags:"Regist" method:"post"  summary:"regist account"`
 	LoginName   string `p:"loginName" v:"required" dc:"user's login name"`
 	Name        string `p:"name" v:"required" dc:"user's name"`
-	Password    string `p:"password" v:"required" dc:"login password, there is going to be encrypt md5"`
-	Repassword  string `p:"repassword" v:"required" dc:"password check"`
+	Password    string `p:"password" v:"password3" dc:"login password, there is going to be encrypt md5"`
+	Repassword  string `p:"repassword" v:"password3" dc:"password check"`
 	Email       string `p:"email" v:"required" dc:"user's email address , all alert message will send to this address"`
 	PhoneNumber string `p:"phoneNumber" v:"required" dc:"telephone number"`
 }
