9235344ac39d796652ca3050cc9bbafb1c4d37c2
leapord <qiyuyang@taisau.com>
2022-09-15 10:30:18 +0800

    优化全局错误处理

--- internal/middleware/global_exception_middleware.go
+++ internal/middleware/global_exception_middleware.go
@@ -1,9 +1,9 @@
 package middleware
 
 import (
+	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
-	"github.com/gogf/gf/v2/text/gstr"
 )
 
 func GlobalExceptionMiddleware(r *ghttp.Request) {
@@ -13,35 +13,11 @@ func GlobalExceptionMiddleware(r *ghttp.Request) {
 	if err == nil {
 		return
 	}
+	code := gerror.Code(err)
 
-	// 重复主键处理
-	if gstr.ContainsI(err.Error(), "Duplicate entry") {
-		src := err.Error()
-		errorString := gstr.SubStr(src, 0, gstr.PosI(src, ","))
-		r.Response.ClearBuffer()
-		r.Response.WriteJson(g.Map{
-			"code":    405,
-			"message": errorString,
-		})
-		return
-	}
-	// 找不到数据
-	if gstr.ContainsI(err.Error(), "sql: no rows in result set") {
-		r.Response.ClearBuffer()
-		r.Response.WriteJson(g.Map{
-			"code":    404,
-			"message": "数据不存在，请检查查询条件",
-		})
-		return
-	}
-
-	// 全局统一错误处理
-	if err != nil {
-		//返回固定的友好信息
-		r.Response.ClearBuffer()
-		r.Response.WriteJson(g.Map{
-			"code":    500,
-			"message": "server busy, please retry after sometimes",
-		})
-	}
+	r.Response.ClearBuffer()
+	r.Response.WriteJson(g.Map{
+		"code":    code.Code(),
+		"message": code.Message(),
+	})
 }
